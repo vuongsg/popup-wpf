c2d7abc985be8dc3497d8c6ffaa4c77f635bb10a
vuongsg <vuong.goteborg@gmail.com>
2021-08-26 19:12:09 +0700

    Let's get started

---
+++ src/PopupWpf/PopupWpf.View/BaseWindow.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace PopupWpf.View
+{
+	public class BaseWindow : Window, INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged(string propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
